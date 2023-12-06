db1dfc3dd5503ed0a3b8319cf6aa88d10a6ccfc7
Tray Keyes <trayton26@gmail.com>
2023-12-05 21:37:46 -0800

    random background color

---
+++ main.js
@@ -0,0 +1,17 @@
+const button = document.getElementById('button');
+
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function changeBackgroundColor() {
+    const background = document.getElementById('background');
+    background.style.backgroundColor = getRandomColor();
+}
+
+button.addEventListener('click', changeBackgroundColor);
